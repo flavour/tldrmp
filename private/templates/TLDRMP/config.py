7d5ae5b3183ba2be3d2b6d00c15025076230512a
Fran Boon <fran@aidiq.com>
2013-02-20 16:11:09 +0000

    TLDRMP: Initial Template/Theme

---
+++ private/templates/TLDRMP/config.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+from gluon import current
+from gluon.storage import Storage
+from gluon.contrib.simplejson.ordered_dict import OrderedDict
+settings = current.deployment_settings
+T = current.T
+
+settings = current.deployment_settings
+T = current.T
+
+"""
+    Template settings for TLDRMP
+"""
+# =============================================================================
+# System Settings
+# -----------------------------------------------------------------------------
+# Authorization Settings
+settings.auth.registration_requires_approval = True
+settings.auth.registration_requires_verification = True
+settings.auth.registration_requests_organisation = True
+settings.auth.registration_organisation_required = True
+settings.auth.registration_requests_site = False
+
+settings.auth.registration_link_user_to = {"staff": T("Staff")}
+
+settings.auth.record_approval = False
+
+settings.auth.registration_roles = {"site_id": ["reader",
+                                                ],
+                                    }
+
+# -----------------------------------------------------------------------------
+# Security Policy
+settings.security.policy = 3 # Controllers
+settings.security.map = True
+
+# Owner Entity
+settings.auth.person_realm_human_resource_site_then_org = False
+
+# -----------------------------------------------------------------------------
+# Pre-Populate
+settings.base.prepopulate = ["TLDRMP"]
+
+settings.base.system_name = T("Disaster Risk Management Portal")
+settings.base.system_name_short = T("DRMP")
+
+# -----------------------------------------------------------------------------
+# Theme (folder to use for views/layout.html)
+settings.base.theme = "TLDRMP"
+#settings.gis.map_height = 600
+#settings.gis.map_width = 854
+
+# -----------------------------------------------------------------------------
+# L10n (Localization) settings
+settings.L10n.languages = OrderedDict([
+    ("en", "English"),
+    #("tet", "Tetum"),
+])
+# Default Language
+settings.L10n.default_language = "en"
+# Default timezone for users
+settings.L10n.utc_offset = "UTC +0900"
+# Number formats (defaults to ISO 31-0)
+# Decimal separator for numbers (defaults to ,)
+settings.L10n.decimal_separator = "."
+# Thousands separator for numbers (defaults to space)
+settings.L10n.thousands_separator = ","
+
+# -----------------------------------------------------------------------------
+# Finance settings
+settings.fin.currencies = {
+    "AUD" : T("Australian Dollars"),
+    "EUR" : T("Euros"),
+    "GBP" : T("Great British Pounds"),
+    "USD" : T("United States Dollars"),
+}
+
+# -----------------------------------------------------------------------------
+# Enable this for a UN-style deployment
+#settings.ui.cluster = True
+# Enable this to use the label 'Camp' instead of 'Shelter'
+settings.ui.camp = True
+
+# -----------------------------------------------------------------------------
+# Save Search Widget
+settings.save_search.widget = False
+
+# =============================================================================
+# Module Settings
+
+# -----------------------------------------------------------------------------
+# Human Resource Management
+# Uncomment to allow Staff & Volunteers to be registered without an email address
+settings.hrm.email_required = False
+# Uncomment to show the Organisation name in HR represents
+settings.hrm.show_organisation = True
+# Uncomment to disable Staff experience
+settings.hrm.staff_experience = False
+# Uncomment to disable the use of HR Credentials
+settings.hrm.use_credentials = False
+# Uncomment to disable the use of HR Skills
+settings.hrm.use_skills = False
+# Uncomment to disable the use of HR Teams
+settings.hrm.use_teams = False
+
+# =============================================================================
+# Template Modules
+# Comment/uncomment modules here to disable/enable them
+settings.modules = OrderedDict([
+    # Core modules which shouldn't be disabled
+    ("default", Storage(
+            name_nice = T("Home"),
+            restricted = False, # Use ACLs to control access to this module
+            access = None,      # All Users (inc Anonymous) can see this module in the default menu & access the controller
+            module_type = None  # This item is not shown in the menu
+        )),
+    ("admin", Storage(
+            name_nice = T("Administration"),
+            #description = "Site Administration",
+            restricted = True,
+            access = "|1|",     # Only Administrators can see this module in the default menu & access the controller
+            module_type = None  # This item is handled separately for the menu
+        )),
+    ("appadmin", Storage(
+            name_nice = T("Administration"),
+            #description = "Site Administration",
+            restricted = True,
+            module_type = None  # No Menu
+        )),
+    ("errors", Storage(
+            name_nice = T("Ticket Viewer"),
+            #description = "Needed for Breadcrumbs",
+            restricted = False,
+            module_type = None  # No Menu
+        )),
+    ("sync", Storage(
+            name_nice = T("Synchronization"),
+            #description = "Synchronization",
+            restricted = True,
+            access = "|1|",     # Only Administrators can see this module in the default menu & access the controller
+            module_type = None  # This item is handled separately for the menu
+        )),
+    ("translate", Storage(
+            name_nice = T("Translation Functionality"),
+            #description = "Selective translation of strings based on module.",
+            module_type = None,
+        )),
+    ("gis", Storage(
+            name_nice = T("Map"),
+            #description = "Situation Awareness & Geospatial Analysis",
+            restricted = True,
+            module_type = 1,     # 1st item in the menu
+        )),
+    ("pr", Storage(
+            name_nice = T("Person Registry"),
+            #description = "Central point to record details on People",
+            restricted = True,
+            access = "|1|",     # Only Administrators can see this module in the default menu (access to controller is possible to all still)
+            module_type = None
+        )),
+    ("org", Storage(
+            name_nice = T("Organizations"),
+            #description = 'Lists "who is doing what & where". Allows relief agencies to coordinate their activities',
+            restricted = True,
+            module_type = None
+        )),
+    # All modules below here should be possible to disable safely
+    ("hrm", Storage(
+            name_nice = T("Staff"),
+            #description = "Human Resources Management",
+            restricted = True,
+            module_type = None,
+        )),
+    ("doc", Storage(
+            name_nice = T("Documents"),
+            #description = "A library of digital resources, such as photos, documents and reports",
+            restricted = True,
+            module_type = None,
+        )),
+    ("msg", Storage(
+            name_nice = T("Messaging"),
+            #description = "Sends & Receives Alerts via Email & SMS",
+            restricted = True,
+            # The user-visible functionality of this module isn't normally required. Rather it's main purpose is to be accessed from other modules.
+            module_type = None,
+        )),
+    ("event", Storage(
+            name_nice = T("Disasters"),
+            #description = "Events",
+            restricted = True,
+            module_type = None
+        )),
+])
